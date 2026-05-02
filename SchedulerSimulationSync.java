b9d3f2253402aff3ff9e71e4457fd0d1555036a4
Faris-Alobaid <445050115@std.psau.edu.sa>
2026-05-02 20:58:02 +0300

    Implemented semaphore to control cpu access

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -142,6 +142,15 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
+        // Acquire cpu access before starting execution
+        try 
+        {
+            SharedResources.cpuSemaphore.acquire();
+        } 
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
         
         try {
             if (startTime == -1) {
@@ -202,9 +211,12 @@ class Process implements Runnable {
             }
             System.out.println();
             
-        } finally {
+        } finally 
+        {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            // Release cpu access after execution
+            SharedResources.cpuSemaphore.release();
         }
     }
     
